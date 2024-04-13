9e9e96099c5c90ba92dee7edcd9d085f88c7cf13
Samuel Bines <samuelBines@gmail.com>
2024-04-13 15:21:44 +0100

    hot reloading

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { streamText, streamSSE } from 'hono/streaming';
 
 const builds = await Bun.build({
   entrypoints: ['./src/main.tsx'],
@@ -23,14 +24,39 @@ app.get('/main.js', async (c) => {
   return c.body(stream, 200, {
     'Content-Type': builds.outputs[0].type, // Correct MIME type, e.g., 'text/javascript'
   });
-
-
 });
 
 // Serve the index.html file
 app.get('/', async (c) => {
   const indexContent = await indexFile.text();
   return c.html(indexContent);
+});
+
+// app.get('/subscribe', (c) => {
+//   return streamText(c, async (stream: any) => {
+//     // Write a process to be executed when aborted.
+//     stream.onAbort(() => {
+//       console.log('Aborted!');
+//     });
+//     // Write a Uint8Array.
+//     await stream.write("hey\n\n");
+//     // Pipe a readable stream.
+//     // await stream.pipe(anotherReadableStream);
+//   });
+// });
+
+let id = 0;
+
+app.get('/watcher', async (c) => {
+  return streamSSE(c, async (stream) => {
+
+    const message = `It is ${new Date().toISOString()}`;
+    await stream.writeSSE({
+      data: message,
+      event: 'reload',
+      id: String(id++),
+    });
+  });
 });
 
 book.get('/', (c) => c.json('Hello Book!'));
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,32 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 
 function App() {
-  return <div> hey there</div>;
+  return <div> Okay cool </div>;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("DOM loaded");
+  var evtSource = new EventSource("/watcher");
+
+
+  evtSource.onmessage = function (event) {
+    console.log("message received", event);
+  };
+
+  evtSource.addEventListener("reload", function (event) {
+    console.log("reload event", event);
+    // location.reload();
+    // Optionally handle the event
+  });
+
+  evtSource.onerror = function (event) {
+    location.reload();
+  };
+});
+
+
 
 // tslint:disable-next-line:no-unused-expression
 const root = createRoot(document.getElementById('app')!);
 root.render(<App />);
+
