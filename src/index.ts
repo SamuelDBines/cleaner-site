00e6759a406d6c95601de818e1a9a950332a0926
Samuel Bines <samuelBines@gmail.com>
2024-04-13 14:13:00 +0100

    added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import { Hono } from 'hono';
 
+const builds = await Bun.build({
+  entrypoints: ['../main.tsx'],
+  target: "browser",
+  minify: {
+    identifiers: true,
+    syntax: true,
+    whitespace: true,
+  },
+
+});
+
+
 const app = new Hono();
-app.get('/', (c) => c.text('Hello Bun!'));
 
 const book = new Hono();
 
 book.get('/', (c) => c.json('Hello Book!'));
 
+
 app.route('/book', book);
+
 
 export default app;
